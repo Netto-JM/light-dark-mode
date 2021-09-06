94e5981f29d0f077a709b414af6f00170c4ac4e9
Netto-JM <32754552+Netto-JM@users.noreply.github.com>
2021-09-05 21:24:04 -0300

    changes for readability and (re)usability

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
+let currentIcon = 'fa-sun';
 
 // Dark or Light Mode Style
-const switchMode = (mode, firstBackgroud, secondBackground, firstIcon, secondIcon) => {
-  nav.style.backgroundColor = firstBackgroud;
-  textBox.style.backgroundColor = secondBackground;
+const switchModeTo = (mode, navigationBackground, textBoxBackground, newIcon) => {
+  nav.style.backgroundColor = navigationBackground;
+  textBox.style.backgroundColor = textBoxBackground;
   toggleIcon.children[0].textContent = `${mode.charAt(0).toUpperCase() + mode.slice(1)} Mode`;
-  toggleIcon.children[1].classList.replace(firstIcon, secondIcon);
+  toggleIcon.children[1].classList.replace(currentIcon, newIcon);
   image1.src = `img/undraw_proud_coder_${mode}.svg`;
   image2.src = `img/undraw_feeling_proud_${mode}.svg`;
   image3.src = `img/undraw_conceptual_idea_${mode}.svg`;
+  currentIcon = newIcon;
 };
 
 // Switch Theme Dynamically
@@ -22,11 +24,11 @@ const switchTheme = event => {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
     localStorage.setItem('theme', 'dark');
-    switchMode('dark', 'rgb(0 0 0 / 50%)', 'rgb(255 255 255 / 50%)', 'fa-sun', 'fa-moon');
+    switchModeTo('dark', 'rgb(0 0 0 / 50%)', 'rgb(255 255 255 / 50%)', 'fa-moon');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
     localStorage.setItem('theme', 'light');
-    switchMode('light', 'rgb(255 255 255 / 50%)', 'rgb(0 0 0 / 50%)', 'fa-moon', 'fa-sun');
+    switchModeTo('light', 'rgb(255 255 255 / 50%)', 'rgb(0 0 0 / 50%)', 'fa-sun');
   }
 };
 
